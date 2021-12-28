a0d4bcbea4d8f87a1cfebb1f271b758be9e48d1b
Pary <81phary@gmail.com>
2021-12-28 22:27:49 +1100

    adding nav and carousel venue

---
+++ app.js
@@ -0,0 +1,36 @@
+window.onscroll = () => {
+    const navbar = document.getElementsByTagName("nav")[0];
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navbar.style.background = "#1b1c1b";
+      } else {
+        navbar.style.background = "none";
+      }
+}
+
+const navSlide = ()=>{
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+
+    //toggle nav
+    burger.addEventListener("click",()=>{
+        nav.classList.toggle("nav-active");
+
+        navLinks.forEach((link,index)=>{
+            if (link.style.animation){
+                link.style.animation="";
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+    
+        //burger animation
+        burger.classList.toggle("toggle");
+    });
+
+
+}
+navSlide();
+
+
